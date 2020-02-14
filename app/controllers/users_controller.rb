8cfd5be72903c892d5046f2f20c8b65674f6eeca
管理者 <sample.com>
2020-02-14 14:40:18 +0000

    Finish add-user-attendances

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,14 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_one_month, only: :show
 
   def index
     @users = User.paginate(page: params[:page])
   end
 
   def show
+    @worked_sum = @attendances.where.not(started_at: nil).count
   end
 
   def new
@@ -56,7 +58,6 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-
   private
 
     def user_params
@@ -66,7 +67,10 @@ class UsersController < ApplicationController
     def basic_info_params
       params.require(:user).permit(:department, :basic_time, :work_time)
     end
-    
+
+    # beforeフィルター
+
+    # paramsハッシュからユーザーを取得します。
     def set_user
       @user = User.find(params[:id])
     end
