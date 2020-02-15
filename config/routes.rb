5592d02f4026ba4148e8c231fba1e69c19e14ebf
管理者 <sample.com>
2020-02-15 08:49:23 +0000

    基本情報設定未実施

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
   resources :users do
     member do
       get 'edit_basic_info'
-      patch 'update_basic_info'
+      get 'update_basic_info'
       get 'attendances/edit_one_month'
-      patch 'attendances/update_one_month' # この行が追加対象です。
+      get 'attendances/update_one_month' 
     end
     resources :attendances, only: :update
   end
